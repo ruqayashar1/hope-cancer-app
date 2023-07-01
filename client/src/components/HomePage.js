83b19d98bd49ed51aadab016489da1bec45f2a70
rukia <ruqayashar1@gmail.com>
2023-07-02 00:13:46 +0300

    HCA-5 homepage

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="homePage">
+            <h1><b>HOPE CANCER APP</b></h1>
+            <h2>WE CARE FOR YOUR UNIQUE NEEDS</h2>
+        </div>
+    );
+}
+
+export default HomePage;
